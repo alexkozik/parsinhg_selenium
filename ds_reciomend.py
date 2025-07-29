c6d4720ef22d34cfbd547eb841405ea06c3c7c5d
alexkozik <alex_kozik@hotmail.com>
2025-07-29 23:00:26 +0300

    selenium firefox troubleshoting

--- ds_reciomend.py
+++ ds_reciomend.py
@@ -1,20 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 import time
 
+
+# Настройки для Selenium с Firefox
+gecko_driver_path = "/snap/bin/geckodriver"  # Укажите путь к geckodriver
+service = Service(gecko_driver_path)
 options = Options()
-options.binary_location = "/usr/bin/firefox"  # Проверенный путь
-
-# Явно указываем путь к geckodriver
-service = Service(executable_path="/usr/local/bin/geckodriver")
-
-options.add_argument("--headless")  # Добавьте эту строку перед созданием driver
-options.add_argument("--disable-gpu")  # На некоторых системах требуется
-options.add_argument("--no-sandbox")  # Для Linux-систем
-options.add_argument("--disable-dev-shm-usage")  # Ограничение памяти
-
+options.add_argument("--headless")  # Запуск в фоновом режиме (без открытия браузера)
 browser = webdriver.Firefox(service=service, options=options)
+
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
 print("Страница загружена:", browser.title)
 time.sleep(5)
