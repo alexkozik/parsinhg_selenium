638d1bd6123bfd1a980ff78c7d57b676ac038dc6
alexkozik <alex_kozik@hotmail.com>
2025-07-29 23:05:44 +0300

    Continue troubleshoting

--- sel_les1.py
+++ sel_les1.py
@@ -6,15 +6,17 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
+# Настройки для Selenium с Firefox
+gecko_driver_path = "/snap/bin/geckodriver"  # Укажите путь к geckodriver
+service = Service(gecko_driver_path)
 options = Options()
-options.binary_location = "/usr/bin/firefox"  # Путь к Firefox (проверьте `which firefox`)
-
-# driver = webdriver.Firefox(options=options)
+options.add_argument("--headless")  # Запуск в фоновом режиме (без открытия браузера)
 
 #Если мы работаем с Firefox
-browser = webdriver.Firefox(options=options)
+browser = webdriver.Firefox(service=service, options=options)
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
 #В кавычках указываем URL сайта, на который нам нужно зайти
+print("Страница загружена:", browser.title)
 time.sleep(10)
 #Задержка в 10 секунд
 browser.quit()
